e83601af0ef09b8ab38cbb7167c553d4dd544196
Adnan Sadik <adnansadik235@gmail.com>
2024-11-03 17:55:59 +0900

    Add files via upload

--- desktop_cleaner.py
+++ desktop_cleaner.py
@@ -4,8 +4,8 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import time
 
-source_dir="C:\Users\PC\Downloads"
-destination_dir="D:\Organized_files"
+source_dir=r"C:\Users\PC\Downloads"
+destination_dir=r"D:\Organized_files"
 File_types={
     "Documents": [".txt",".pdf",".docx",".doc"],
     "Images": [".jpg",".jpeg",".png",".gif"],
@@ -14,32 +14,54 @@ File_types={
     "Zipfiles": [".zip",".rar",".tar", ".gz"]
 
 }
+def wait(file_path):
+    while True:
+        try:
+            with open(file_path,'r'):
+                break
+        except PermissionError:
+            if file_path.endswith('.tmp'):
+                time.sleep(5)
+            else:
+                time.sleep(10)
 
-def oragnize_files(file_path):
-    name,ext=os.path.splitext(file_path)
-    ext=ext.lower()
+
+def organize_files_in_destination():
+    for file_name in os.listdir(source_dir):
+        file_path=os.path.join(source_dir,file_name)
+        if os.path.isfile(file_path):
+            organize_files(file_path)
+
+def organize_files(file_path):
+    try:
+        wait(file_path)
+        name,ext=os.path.splitext(file_path)
+        ext=ext.lower()
 
 
-    for folder_name,extensions in File_types.items():
-        if ext in extensions:
-            destination_folder=os.path.join(destination_dir,folder_name)
-            os.makedirs(destination_folder,exist_ok=True)
-            shutil.move(file_path, os.path.join(destination_folder,os.path.basename(file_path)))
-            print(f"moved '{os.path.basename(file_path)}' from '{file_path}' to '{destination_folder}'")
-            return
+        for folder_name,extensions in File_types.items():
+            if ext in extensions:
+                destination_folder=os.path.join(destination_dir,folder_name)
+                os.makedirs(destination_folder,exist_ok=True)
+                shutil.move(file_path, os.path.join(destination_folder,os.path.basename(file_path)))
+                print(f"moved '{os.path.basename(file_path)}' from '{file_path}' to '{destination_folder}'")
+                return
 
         others=os.path.join(destination_dir,"others")
         os.makedirs(others, exist_ok= True)
         shutil.move(file_path,os.path.join(others,os.path.basename(file_path)))
         print(f"moved '{os.path.basename(file_path)}' from '{file_path}' to '{others}'")
+    except Exception as e:
+        print(f"found exception but moving on'{e}'")
 
 
 
 # handling events
 class File_Organizer_Handler(FileSystemEventHandler):
-    def created(self,event):
+    def on_created(self,event):
         if not event.is_directory: #explicitly handles files not folders or directories
-            organize_files(event.src_path)
+            time.sleep(10)
+            organize_files_in_destination()
 
 observer=Observer()
 
@@ -51,7 +73,7 @@ observer.start()
 print("Observation started. Press Ctrl+C to stop")
 
 try:
-    while true:
+    while True:
         time.sleep(1)
 except KeyboardInterrupt:
     observer.stop()
